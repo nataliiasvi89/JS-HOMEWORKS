8d905f6c586a8171a8371de7cd329e1944f9afaf
nataliiasvi89 <nataliasvi89@gmail.com>
2025-11-24 03:13:15 +0100

    dz #4 completed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const price = prompt("Початкова ціна товару");
+const discount = prompt("Розмір знижки у відсотках (наприклад, 10, 15, 20)");
+if (Number(discount) > 0
+    && Number(discount) !== NaN
+    && Number(price) > 0
+    && Number(price) !== NaN) {
+    const result = Number(price) - (Number(price) * Number(discount) / 100);
+    alert(`Початкова ціна: ${price} грн, знижка: ${discount}%. Ціна зі знижкою: ${result} грн.`)
+} else alert("Помилка вводу");

81b6beb9df6c0dd14857cc5805ff9dd595982775
nataliiasvi89 <nataliasvi89@gmail.com>
2025-11-28 15:25:27 +0100

    hw #7 completed

---
+++ js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let age = prompt("Введіть ваш вік:");
+age = Number(age.trim());
+if (age <= 0 || age > 120 || isNaN(age) || age === " ") {
+    alert("Некоректний вік");
+} else alert("Через 5 років вам буде: " + (age + 5));
+
+const price1 = "120.50$";
+const price2 = "UAH 999";
+const height = "180cm";
+const broken = "abc123";
+
+console.log(
+    `${price1} -> parseInt: ${parseInt(price1)}, parseFloat: ${parseFloat(price1)}`
+);
+console.log(
+    `${price2} -> parseInt: ${parseInt(price2)}, parseFloat: ${parseFloat(price2)}`
+);
+console.log(
+    `${height} -> parseInt: ${parseInt(height)}, parseFloat: ${parseFloat(height)}`
+);
+console.log(
+    `${broken} -> parseInt: ${parseInt(broken)}, parseFloat: ${parseFloat(broken)}`
+);
+
+// If the first character is not a number, then it will be Nan
